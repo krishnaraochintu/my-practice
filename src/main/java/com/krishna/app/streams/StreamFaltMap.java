515faeff39af8944babc424718983d66cedc9b91
Krishna Rao Chintu <krishna.chintu@wavemaker.com>
2021-07-24 13:49:37 +0530

    Streams API

--- src/main/java/com/krishna/app/streams/StreamFaltMap.java
+++ src/main/java/com/krishna/app/streams/StreamFaltMap.java
@@ -1,10 +1,33 @@
 package com.krishna.app.streams;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StreamFaltMap {
     public static void main(String[] args) {
-        List citylist = Arrays.asList("delhi", "mumbai", "hyderabad", "ahmedabad", "indore", "patna");
+
         List<Integer> numbersList = Arrays.asList(12, 45, 67, 19, 87, 2, 9);
-        numbersList.stream()
+        numbersList.stream().map(integer -> integer * 2).forEach(System.out::println);
+        List<String> productlist1 = Arrays.asList("Printer", "Mouse", "Keyboard", "Motherboard");
+        List<String> productlist2 = Arrays.asList("Scanner", "Projector", "Light Pen");
+        List<String> productlist3 = Arrays.asList("Pen Drive", "Charger", "WIFI Adapter", "Cooling Fan");
+        List<String> productlist4 = Arrays.asList("CPU Cabinet", "WebCam", "USB Light", "Microphone", "Power cable");
+        List<List<String>> allproducts = new ArrayList<List<String>>();
+        allproducts.add(productlist1);
+        allproducts.add(productlist2);
+        allproducts.add(productlist3);
+        allproducts.add(productlist4);
+        List<String> listOfAllProducts = new ArrayList<String>();
+        for (List<String> pro : allproducts) {
+            for (String product : pro) {
+                listOfAllProducts.add(product);
+            }
+        }
+        allproducts.stream().flatMap(pList -> pList.stream()).collect(Collectors.toList());
+        allproducts.stream().collect(Collectors.toList());
     }
 }
