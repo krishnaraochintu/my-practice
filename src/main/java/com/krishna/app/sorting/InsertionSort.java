1dd5533895dc8757d04b64cbd8bf4315cef3998a
Krishna Rao Chintu <krishna.chintu@wavemaker.com>
2021-12-27 14:17:06 +0530

    Insertion Sort

--- src/main/java/com/krishna/app/sorting/InsertionSort.java
+++ src/main/java/com/krishna/app/sorting/InsertionSort.java
@@ -2,11 +2,13 @@ package com.krishna.app.sorting;
 
 public class InsertionSort {
     //Time Complexity 0(n2)
-    void sort(int arr[])  {
+    // Space Complexity 0(1)
+    //Sort each element with in left of its. like fixing cards
+    void insertionSort(int arr[]) {
         for(int i =1; i < arr.length; i++) {
             int key = arr[i];
-            int j = i - 1;
-            while(j < 0 && arr[j] > key) {
+            int j = i -1;
+            while (j >=0 && arr[j] > key) {
                 arr[j+1] = arr[j];
                 j--;
             }
